fd6219eea48416fbd364960dcfc0042c1cae0ad0
bicpa320 <bicpa320@school.lu>
2019-11-21 08:26:27 +0100

    no message

--- codename_geography/Intermediate/Build/Win64/codename_geographyEditor/Development/Engine/SharedPCH.Engine.cpp
+++
@@ -1,1 +0,0 @@
-#include "Z:/BTS/OneDrive - 365education/GameJams/Powerweek2019/github_project/repo/codename_geography/Intermediate/Build/Win64/codename_geographyEditor/Development/Engine/SharedPCH.Engine.h"
--- codename_geography/Source/codename_geography/codename_geography.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
-
-#include "codename_geography.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, codename_geography, "codename_geography" );
-
-DEFINE_LOG_CATEGORY(Logcodename_geography)
- 
